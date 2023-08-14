3a2ecc7782b0d7f8b4ef2130cb578e8299c0b396
pustart <polaris9955@gmail.com>
2023-08-14 07:40:05 +0300

    feat: add prompts for players names

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,27 @@
 import DomController from "./Dom.controller.mjs";
 import Game from "./Game.mjs";
+import Player from './Player.mjs';
+import {
+  FIRST_PLAYER_NAME,
+  SECOND_PLAYER_NAME,
+  CROSS_SYMBOL,
+  ZERO_SYMBOL,
+} from "./constants.mjs";
 
-const game = new Game();
+
+const player1Name = prompt("Enter Player 1 name:") || FIRST_PLAYER_NAME;
+const player2Name = prompt("Enter Player 2 name:") || SECOND_PLAYER_NAME;
+const firstTurn = prompt("Enter name of player with first turn:") || FIRST_PLAYER_NAME;
+
+let player1 = new Player(player1Name, CROSS_SYMBOL);
+let player2 = new Player(player2Name, ZERO_SYMBOL);
+
+if (firstTurn === player2Name) {
+  player1 = new Player(player2Name, CROSS_SYMBOL);
+  player2 = new Player(player1Name, ZERO_SYMBOL);
+}
+
+const game = new Game(player1, player2);
 const dom = new DomController({
   root: 'body',
   game,
